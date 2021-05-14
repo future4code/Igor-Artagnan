92ab253cbbf47e04adb076ee49f530661b0e0f2c
artagnancoder <stayhomer@outlook.com>
2021-05-14 15:51:13 -0300

    Projeto realizado! Desafio em andamento.

---
+++ semana2/blackjack/nossoJogo.js
@@ -0,0 +1,48 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+let somaCartas = (numero1, numero2) => {
+   return numero1 + numero2
+}
+
+let carta1 = comprarCarta()
+let carta2 = comprarCarta()
+let carta3 = comprarCarta()
+let carta4 = comprarCarta()
+
+
+
+console.log("Bem vindo ao jogo de BlackJack")
+if (confirm(`Quer iniciar uma nova rodada?`) === true) {
+   
+   let somaUsuario = somaCartas(carta1.valor, carta2.valor)
+   let somaComputador = somaCartas(carta3.valor, carta4.valor)
+   
+   console.log(`Usuário - cartas - ${carta1.texto} ${carta2.texto} - Pontuação ${somaUsuario}`)
+   console.log(`Computador - cartas - ${carta3.texto} ${carta4.texto} - Pontuação ${somaComputador}`)
+
+   if (somaUsuario > somaComputador) {
+      console.log("O usuário ganhou!")
+   } else if (somaComputador > somaUsuario) {
+      console.log("O computador ganhou!")
+   } else {
+      console.log("Empate!")
+   }
+
+
+
+
+} else {
+   console.log(`O jogo acabou!`)
+}
+
+
