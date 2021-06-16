e777ab8429bb7bac5ae9ba6b8be17cf6fc4fa600
artagnancoder <stayhomer@outlook.com>
2021-06-16 13:44:29 -0300

    Implementaçaõ de duas telas

--- semana7/labenusers/labenusers/src/App.js
+++ semana7/labenusers/labenusers/src/App.js
@@ -1,143 +1,32 @@
 import React from "react";
-import axios from "axios";
-import styled from "styled-components"
+import TelaListUsuario from "./components/TelaListaUsuario";
+import TelaCadastro from "./components/TelaCadastro";
 
-const Container = styled.div`
-border: 1px solid black;
-display: flex;
-align-items: center;
-flex-direction: column;
-width: 50%;
-margin: auto;
-margin-top: 49px;
-
-`
-
-const BotaoUsuarios = styled.button`
-margin-top: 19px;
-
-`
-
-const BotaoAdicionar = styled.button`
-margin-top: 14px;
-`
-
-
-const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
-
-const header = {
-  headers: {
-    Authorization: "igor-molina"
-  }
-}
-
-
-
-export default class Home extends React.Component {
+export default class App extends React.Component {
   state = {
-    
-    usuarios: [],
-
-    inputName: "",
-    inputEmail: "",
-  
+    telaAtual: "cadastro"
   };
 
- 
-
-  onChangeName = (event) => {
-    this.setState({ inputName: event.target.value })
-  }
-
-  onChangeEmail = (event) => {
-    this.setState({ inputEmail: event.target.value })
-  }
-
-  criarUsuario = () => {
-
-    const body = {
-      name: this.state.inputName,
-      email: this.state.inputEmail
-  
+  escolheTela = () => {
+    switch (this.state.telaAtual) {
+      case "cadastro":
+        return <TelaCadastro irParaLista={this.irTelaLista} />;
+      case "lista":
+        return <TelaListUsuario irParaCadastro={this.irTelaCadastro} />;
+      default:
+        return <div>Página não encontrada.</div>;
     }
+  };
 
+  irTelaCadastro = () => {
+    this.setState({ telaAtual: "cadastro" });
+  };
 
-    axios.post(url, body, header)
-
-      .then(() => {
-        alert("Usuário cadastrado com sucesso!")
-    
-        this.setState({inputName: "", inputEmail:"" })
-        
-        this.mostrausuarios()
-      })
-
-      .catch((error) => {
-        alert(error.response.data.message)
-      })
-  }
-
-  mostrausuarios = () =>{
-
-    axios.get(url, header)
-    .then((response)=>{
-
-      this.setState({usuarios: response.data})
-
-
-
-    })
-
-    .catch((error) =>{
-      alert(error.response.data.message)
-    })
-
-  }
-
-  
-  
+  irTelaLista = () => {
+    this.setState({ telaAtual: "lista" });
+  };
 
   render() {
-    
-    
-
-       const listaDeUsuarios = this.state.usuarios.map((usuario)=>{
-        return <p key={usuario.id}>
-              
-              {usuario.name}
-              
-              </p>
-
-      }) 
-
-
-
-     
-
-      
-
-      
-    return (
-      <Container>
-        <h3>Nome:</h3>
-        <input type="text" 
-        value={this.state.inputName} 
-        onChange={this.onChangeName} />
-
-        <h3>Email:</h3>
-        <input type="text" 
-        value={this.state.inputEmail} 
-        onChange={this.onChangeEmail} />
-        
-        <BotaoAdicionar onClick={this.criarUsuario}>Registrar</BotaoAdicionar>
-        <BotaoUsuarios>Ver usuários cadastrados</BotaoUsuarios>
-          
-          
-          <h3>Usuários Cadastrados:</h3>
-          {listaDeUsuarios}
-         
-
-      </Container>
-    );
+    return <div>{this.escolheTela()}</div>;
   }
 }
---
+++ semana7/labenusers/labenusers/src/components/TelaListaUsuario.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import React from "react";
+
+export default class TelaListaUsuario extends React.Component {
+  state = {
+    usuarios: []
+  };
+
+  componentDidMount() {
+    this.getAllUsers();
+  }
+
+  getAllUsers = () => {
+    const url =
+      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
+    const header = {
+      headers: {
+        Authorization: "igor-molina"
+      }
+    };
+
+    axios
+      .get(url, header)
+
+      .then((response) => {
+        this.setState({ usuarios: response.data });
+      })
+
+      .catch((error) => {
+        alert("Erro, tente novamente!");
+      });
+  };
+
+  deleteUser = (id) => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`;
+
+    axios
+      .delete(url, {
+        headers: {
+          Authorization: "igor-molina"
+        }
+      })
+
+      .then((response) => {
+        alert("Usuário deletado com sucesso!");
+        this.getAllUsers();
+      })
+
+      .catch((error) => {
+        alert("Ocorreu um erro, tente novamente!");
+      });
+  };
+
+ 
+
+  render() {
+    const listaUsuarios = this.state.usuarios.map((usuario) => {
+      return (
+        <div key={usuario.id}>
+          {usuario.name}
+          <button onClick={() => this.deleteUser(usuario.id)}>Apagar</button>
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <button onClick={this.props.irParaCadastro}>Cadastrar Usuário</button>
+        <h3>Lista de usuários</h3>
+        {listaUsuarios}
+      </div>
+    );
+  }
+}
