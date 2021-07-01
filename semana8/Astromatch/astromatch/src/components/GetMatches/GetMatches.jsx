c4f0949d918b67f40796afb850d597939c5868b4
artagnancoder <stayhomer@outlook.com>
2021-07-01 09:16:19 -0300

    Inicio da implementação do endpoint getMatches

--- semana8/Astromatch/astromatch/src/components/GetMatches/GetMatches.jsx
+++ semana8/Astromatch/astromatch/src/components/GetMatches/GetMatches.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
+import { BASE_URL } from '../../constantes/url'
+
 
 function GetMatches() {
+
+    const [arrayMatches, setArrayMatches] = useState([])
+
+const getMatches = () =>{
+    axios(`${BASE_URL}/matches`) 
+    
+    .then((response)=>{
+        setArrayMatches(response.data)
+    }) 
+    
+    .catch((error)=>{
+        console.log(error)
+    }) 
+}
+console.log('matches', arrayMatches)
     return (
         <div>
-            <button>Visualizar Matches</button>
+            <button onClick={getMatches}>Visualizar Matches</button>
         </div>
     )
 }
