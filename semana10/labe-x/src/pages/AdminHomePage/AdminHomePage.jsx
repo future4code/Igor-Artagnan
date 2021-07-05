f951904b1298c1fba609890ee7376492c052bc25
artagnancoder <stayhomer@outlook.com>
2021-07-05 16:15:50 -0300

    Reorganização dos caminhos de importação dos componentes

--- semana10/labe-x/src/pages/AdminHomePage/AdminHomePage.jsx
+++ semana10/labe-x/src/pages/AdminHomePage/AdminHomePage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import ApplicationFormPage from '../ApplicationFormPage/ApplicationFormPage'
+import CreateTripPage from '../CreateTripPage/CreateTripPage'
+import HomePage from '../HomePage/HomePage'
+import ListTripsPage from '../ListTripsPage/ListTripsPage'
+import LoginPage from '../LoginPage/LoginPage'
+import TripDetailsPage from '../TripDetailsPage/TripDetailsPage'
 
 export default function  AdminHomePage(){
 return (
     <div>
-        AdminHomePage
+       {/*  <ApplicationFormPage/>
+        <CreateTripPage/>
+        <HomePage/>
+        <ListTripsPage/>
+        <LoginPage/>
+        <TripDetailsPage/> */}
     </div>
 )
 }
