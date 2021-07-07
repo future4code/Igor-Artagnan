2f44a4af8f60a119b7821b1e9f43c30d75431a91
artagnancoder <stayhomer@outlook.com>
2021-07-07 11:28:42 -0300

    Criação dos inputs controlados nos arquivos CreateTripPage.jsx e ApplicationFormPage.jsx

--- semana10/labe-x/src/pages/ApplicationFormPage/ApplicationFormPage.jsx
+++ semana10/labe-x/src/pages/ApplicationFormPage/ApplicationFormPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { MainContainer, FormDiv, InfoDiv, StyledButton, ButtonsDiv, StyledInput, StyledSelect } from './styled'
 import { useHistory } from 'react-router-dom'
 
@@ -8,6 +8,34 @@ export default function ApplicationFormPage() {
     const goBack = () => {
         history.goBack()
     }
+
+    const [name, setName] = useState("")
+    const [age, setAge] = useState("") 
+    const [applicationText, setApplicationText ] = useState("")
+    const [profession, setProfession] = useState("")
+    const [country, setCountry] = useState("")
+    
+    const onChangeName = (e) => {
+        setName(e.target.value)
+    }
+
+    const onChangeAge = (e) => {
+        setAge(e.target.value)
+    }
+
+    const onChangeApplicationText = (e) => {
+        setApplicationText(e.target.value)
+    }
+
+    const onChangeProfession = (e) => {
+        setProfession(e.target.value)
+    }
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    console.log(name, age, applicationText, profession)
 
     return (
         <MainContainer>
@@ -19,11 +47,33 @@ export default function ApplicationFormPage() {
                         <option>exemplo</option>
                         <option>exemplo</option>
                     </StyledSelect>
-                    <StyledInput placeholder={'Nome'} />
-                    <StyledInput placeholder={'Idade'} />
-                    <StyledInput placeholder={'Texto de candidatura'} />
-                    <StyledInput placeholder={'Profissão'} />
-                    <StyledSelect>
+                    <StyledInput 
+                    placeholder={'Nome'} 
+                    value={name}
+                    onChange={onChangeName}
+                    />
+                    <StyledInput 
+                    placeholder={'Idade'} 
+                    value={age}
+                    onChange={onChangeAge}
+
+                    />
+                    <StyledInput 
+                    placeholder={'Texto de candidatura'} 
+                    value={applicationText}
+                    onChange={onChangeApplicationText}
+                    />
+                    <StyledInput 
+                    placeholder={'Profissão'} 
+                    value={profession}
+                    onChange={onChangeProfession}
+
+                    />
+                    <StyledSelect
+                    value={country}        //Verificar onChange do select
+                    onChange={onChangeCountry}
+                    
+                    >
                         <option>exemplo</option>
                         <option>exemplo</option>
                         <option>exemplo</option>
--- semana10/labe-x/src/pages/CreateTripPage/CreateTripPage.jsx
+++ semana10/labe-x/src/pages/CreateTripPage/CreateTripPage.jsx
@@ -1,33 +1,76 @@
-import React from 'react'
-import {useHistory} from 'react-router-dom'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import { MainContainer, ButtonsDiv, StyledButton, InfoDiv, FormDiv, StyledInput, StyledSelect } from './styled'
 
 export default function CreateTripPage() {
     const history = useHistory()
 
-    const goBack = () =>{
+    const goBack = () => {
         history.goBack()
     }
+
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [planet, setPlanet] = useState("")
+    const [durationInDays, setDurationInDays] = useState("")
+    const [date, setDate] = useState("")
+
+    const onChangeName = (e) => {
+        setName(e.target.value)
+    }
+
+    const onChangeDescription = (e) => {
+        setDescription(e.target.value)
+    }
+
+    const onChangePlanet = (e) => {
+        setPlanet(e.target.value)
+    }
+
+    const onChangeDurationInDays = (e) => {
+        setDurationInDays(e.target.value)
+    }
+
+    const onChangeDate = (e) => {
+        setDate(e.target.value)
+    }
+
+
+
     return (
         <MainContainer>
             <InfoDiv>
-            <h1>Cadastrar viagem</h1>
-            <FormDiv>
-            <StyledInput placeholder={"Título"}/>
-            <StyledSelect>
-                <option>Escolha um Planeta</option>
-                <option>Exemplo</option>
-                <option>Exemplo</option>
-            </StyledSelect>
-            <StyledInput type="date"/>
-            <StyledInput placeholder={"Descrição"}/>
-            <StyledInput placeholder={"Duração em dias"}/>
-            </FormDiv>
+                <h1>Cadastrar viagem</h1>
+                <FormDiv>
+                    <StyledInput
+                        placeholder={"Título"}
+                        onChange={onChangeName}
+                    />
+                    <StyledSelect
+                        onChange={onChangePlanet}
+                    >
+                        <option>Escolha um Planeta</option>
+                        <option>Exemplo</option>
+                        <option>Exemplo</option>
+                    </StyledSelect>
+                    <StyledInput
+                        type="date"
+                        onChange={onChangeDate}
+                    />
+                    <StyledInput
+                        placeholder={"Descrição"}
+                        onChange={onChangeDescription}
+                    />
+                    <StyledInput
+                        placeholder={"Duração em dias"}
+                        onChange={onChangeDurationInDays}
+                    />
+                </FormDiv>
 
-            <ButtonsDiv>
-            <StyledButton onClick={goBack}>Voltar</StyledButton>
-            <StyledButton>Criar</StyledButton>
-            </ButtonsDiv>
+                <ButtonsDiv>
+                    <StyledButton onClick={goBack}>Voltar</StyledButton>
+                    <StyledButton>Criar</StyledButton>
+                </ButtonsDiv>
             </InfoDiv>
         </MainContainer>
     )
