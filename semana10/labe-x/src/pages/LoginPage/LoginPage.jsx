4efe751e4b4543a9a6f8440c26ca6be144193515
artagnancoder <stayhomer@outlook.com>
2021-07-07 11:00:57 -0300

    Implementação dos inputs controlados na tela de login

--- semana10/labe-x/src/pages/LoginPage/LoginPage.jsx
+++ semana10/labe-x/src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { MainContainer, InputsContainer, StyledInputs, ButtonDiv, InfoDiv, StyledButton } from './styled'
 
 export default function LoginPage() {
-    
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     const history = useHistory()
 
     const goBack = () => {
@@ -13,18 +16,35 @@ export default function LoginPage() {
     const renderAdminHomePage = () => {
         history.push("AdminHomePage")
     }
+
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const onChangePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
     return (
         <MainContainer>
             <InfoDiv>
-            <h1>Login</h1>
-            <InputsContainer>
-                <StyledInputs placeholder={'email'} />
-                <StyledInputs placeholder={'senha'} type={'password'} />
-            </InputsContainer>
-            <ButtonDiv>
-                <StyledButton onClick={goBack}>Voltar</StyledButton>
-                <StyledButton onClick={renderAdminHomePage}>Entrar</StyledButton>
-            </ButtonDiv>
+                <h1>Login</h1>
+                <InputsContainer>
+                    <StyledInputs
+                        placeholder={'email'}
+                        value={email}
+                        onChange={onChangeEmail}
+                    />
+                    <StyledInputs
+                        placeholder={'senha'}
+                        value={password}
+                        onChange={onChangePassword}
+                        type={'password'} />
+                </InputsContainer>
+                <ButtonDiv>
+                    <StyledButton onClick={goBack}>Voltar</StyledButton>
+                    <StyledButton onClick={renderAdminHomePage}>Entrar</StyledButton>
+                </ButtonDiv>
             </InfoDiv>
         </MainContainer>
     )
