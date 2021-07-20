82e9c6243ac9bf875c3bf0c3142cae0c91691f8a
artagnancoder <stayhomer@outlook.com>
2021-07-20 09:31:51 -0300

    Criação do hook useForm e início da implementação da tela de login

--- semana11/labeddit/src/pages/LoginPage/LoginPage.jsx
+++ semana11/labeddit/src/pages/LoginPage/LoginPage.jsx
@@ -2,36 +2,42 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
+import { MainContainer, InputsContainer } from './styled'
+
 const LoginPage = () => {
 
-  return <div>
-    <form>
-      <TextField
-        id="standard-multiline-flexible"
-        label="Email"
-        multiline
-        maxRows={4}
+  return <MainContainer>
+    
+      <form>
+        <InputsContainer>
+          <TextField
+            id="standard-multiline-flexible"
+            label="Email"
+            multiline
+            maxRows={4}
+            required
+          />
 
-      />
+          <TextField
+            id="standard-multiline-flexible"
+            label="Senha"
+            type="password"
+            multiline
+            maxRows={4}
+            required
+          />
 
-      <TextField
-        id="standard-multiline-flexible"
-        label="Senha"
-        type="password"
-        multiline
-        maxRows={4}
+        </InputsContainer>
 
-      />
-
-      <Button variant="contained" color="primary" disableElevation>
-        Login
+        <Button variant="contained" color="primary" disableElevation>
+          Login
     </Button>
-    </form>
-
+      </form>
+   
     <Button variant="contained" color="primary" disableElevation>
       Cadastre-se
     </Button>
-  </div>
+  </MainContainer>
 }
 
 export default LoginPage
---
+++ semana11/labeddit/src/pages/LoginPage/styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+
+const MainContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 100vw;
+margin-top: 10vh;
+`
+const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  max-width: 450px;
+  align-items: center;
+  margin-bottom: 20px;
+`
+
+export {
+  MainContainer,
+  InputsContainer,
+  
+}
