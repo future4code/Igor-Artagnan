ecb3ff543a643caccfe0defef496df24b0fd3685
artagnancoder <stayhomer@outlook.com>
2021-07-21 10:15:04 -0300

    Implementação do endpoint get post comments e redenrização dos comentários

--- semana11/labeddit/src/pages/FeedPage/FeedPage.jsx
+++ semana11/labeddit/src/pages/FeedPage/FeedPage.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import PostCard from '../../components/PostCard/PostCard';
 import useProtectedPage from '../../hooks/useProtectedPage'
 import useRequestData from '../../hooks/useRequestData';
+import { goToPostPage } from '../../routes/coordinator'
 import { BASE_URL } from '../../constants/url'
-import {MainContainer} from './styled'
+import { useHistory } from 'react-router-dom'
+import { MainContainer } from './styled'
 
 
 const FeedPage = () => {
 
   useProtectedPage()
+
   const posts = useRequestData([], `${BASE_URL}/posts`)
+  const history = useHistory()
+
+  const onClickCard = (id) => {
+    goToPostPage(history, id)
+  }
 
 
   const postCards = posts.map((post) => {
     return <PostCard
-      onClick={() => null}
+      onClick={() => onClickCard(post.id)}
       key={post.id}
       username={post.username}
       title={post.title}
@@ -25,7 +33,7 @@ const FeedPage = () => {
 
   return <MainContainer>
 
-    
+
 
     {postCards}
 
--- semana11/labeddit/src/pages/PostPage/PostPage.jsx
+++ semana11/labeddit/src/pages/PostPage/PostPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import useProtectedPage from '../../hooks/useProtectedPage'
+import { useParams } from 'react-router-dom'
+import useRequestData from '../../hooks/useRequestData';
+import { BASE_URL } from '../../constants/url';
 
 
 const PostPage = () => {
   useProtectedPage()
+
+  const params = useParams()
+  console.log(params)
+
+  const postComents = useRequestData([], `${BASE_URL}/posts/${params.id}/comments`)
+
+  console.log('COMENTARIOS', postComents)
+
   return <div>
     PostPage
   </div>
