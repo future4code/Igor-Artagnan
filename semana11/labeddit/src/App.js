ce8f17518524265f5cc8380aa615e7d33b7bc83c
artagnancoder <stayhomer@outlook.com>
2021-07-23 09:49:47 -0300

    Criação do global style

--- semana11/labeddit/src/App.js
+++ semana11/labeddit/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Router from './routes/Router'
+import { createGlobalStyle } from 'styled-components'
 
 
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    background-color: white;
+  }
+  li{
+    list-style: none;
+  }
+`
 const App = () => {
 
   return <div>
-    <Router/>
+    <GlobalStyle />
+    <Router />
   </div>
 }
 
