1a9e6089b4cdf2b7d2fe51f7b468d6b47c5b69ab
artagnancoder <stayhomer@outlook.com>
2021-08-13 13:44:42 -0300

    Criação do mock do banco de dados

--- semana15/sistemaBancario/src/index.ts
+++ semana15/sistemaBancario/src/index.ts
@@ -1,6 +1,29 @@
-import express, {Express} from 'express'
+import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 import { AddressInfo } from "net";
+
+type user = {
+    name: string,
+    cpf: number,
+    birthDate: number,
+    balance: number, 
+}
+
+type spent = [{
+    value: number,
+    date: number,
+    description: string
+}]
+
+const registeredUsers: user[]= [
+    {
+        name: "Igor",
+        cpf: 13247845,
+        birthDate: 120197,
+        balance: 50000
+
+    }
+]
 
 const app: Express = express();
 
@@ -8,7 +31,14 @@ app.use(express.json());
 app.use(cors());
 
 
-
+app.get('/users', (req: Request, res: Response) => {
+    let codeError: number = 404
+    try {
+        res.status(200).send(registeredUsers)
+    } catch (error) {
+        res.status(codeError).send({ message: error.message })
+    }
+})
 
 
 
