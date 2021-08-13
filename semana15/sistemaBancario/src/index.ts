78618e416cba5ab60c0df9488fbe3b3e7c3d5c4d
artagnancoder <stayhomer@outlook.com>
2021-08-13 18:50:47 -0300

    Código da sugestão de correção

--- semana15/sistemaBancario/src/index.ts
+++ semana15/sistemaBancario/src/index.ts
@@ -1,42 +1,61 @@
-import express, { Express, Request, Response } from 'express'
-import cors from 'cors'
+//Código da sugestão de resolução
+
+import express, { Request, Response, Express } from "express";
+import cors from "cors";
 import { AddressInfo } from "net";
+import { accounts } from "./accounts";
 
-type user = {
-    name: string,
-    cpf: number,
-    birthDate: number,
-    balance: number, 
-}
-
-type spent = [{
-    value: number,
-    date: number,
-    description: string
-}]
-
-const registeredUsers: user[]= [
-    {
-        name: "Igor",
-        cpf: 13247845,
-        birthDate: 120197,
-        balance: 50000
-
-    }
-]
 
 const app: Express = express();
-
 app.use(express.json());
 app.use(cors());
 
-
-app.get('/users', (req: Request, res: Response) => {
-    let codeError: number = 404
+app.post("/users/create", (req: Request, res: Response) => {
     try {
-        res.status(200).send(registeredUsers)
+        const { name, CPF, dateOfBirthAsString } = req.body
+
+        const [day, month, year] = dateOfBirthAsString.split("/")
+
+        const dateOfBirth: Date = new Date(`${year}-${month}-${day}`)
+
+        //validar as entradas da requisição
+
+        const ageInMilisseconds: number = Date.now() - dateOfBirth.getTime()
+
+        const ageInYears: number = ageInMilisseconds / 1000 / 60 / 60 / 24 / 365
+
+        if(ageInYears < 18){
+            res.statusCode = 406
+            throw new Error("Idade deve ser maior que 18 anos")
+        }
+
+        //consultar ou alterar a base de dados
+       
+        accounts.push({ name, CPF, dateOfBirth, balance: 0, statement: [] })
+
+
+        //validar os resultados da consulta
+        //enviar a resposta
+        res.status(201).send('Conta criada com sucesso!')
+
     } catch (error) {
-        res.status(codeError).send({ message: error.message })
+        res.status(400).send(error.message)
+
+    }
+})
+
+app.get('/users/all', (req: Request, res: Response) => {
+    try {
+        if(!accounts.length){
+            res.statusCode = 404
+            throw new Error("Nenhuma conta encontrada.")
+        }
+        res.status(200).send(accounts)
+
+    } catch (error) {
+        res.send(error.message)
+
+
     }
 })
 
@@ -46,11 +65,13 @@ app.get('/users', (req: Request, res: Response) => {
 
 
 
+
+
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost: ${address.port}`);
     } else {
-       console.error(`Failure upon starting server.`);
+        console.error(`Failure upon starting server.`);
     }
 });
