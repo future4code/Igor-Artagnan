586d0359fc3e6de08396757e52cac7d04d3724da
artagnancoder <stayhomer@outlook.com>
2021-08-18 17:08:45 -0300

    Desafios

--- semana16/knexJS/src/index.ts
+++ semana16/knexJS/src/index.ts
@@ -136,6 +136,70 @@ app.delete("/actor/:id", async (req:Request, res: Response) => {
     }
 })
 
+const createMovie = async (
+    id: string,
+    title: string,
+    synopsis: string,
+    releaseDate: Date,
+    playingLimitDate: Date
+) => {
+    await connection
+    .insert({id: id, title: title, synopsis: synopsis, release_date: releaseDate, playing_limt_date: playingLimitDate})
+
+    .into("Movie")
+}
+
+app.post("/movie", async (req: Request, res: Response) => {
+    try {
+        await createMovie(
+            req.body.id,
+            req.body.title,
+            req.body.synopsis,
+            req.body.releaseDate,
+            req.body.playingLimitDate
+        )
+
+        res.status(200).send({message: "Success"})
+    } catch (err) {
+        res.status(400).send({message: err.message})
+    }
+})
+
+const getAllMovies = async (): Promise<any> => {
+    const result = await connection.raw(`
+      SELECT * FROM Movie LIMIT 15
+    `);
+  
+    return result[0];
+  };
+  
+  app.post("/movie/:id", async (req: Request, res: Response) => {
+    try {
+      const movies = await getAllMovies();
+  
+      res.status(200).send({
+        movies: movies,
+      });
+    } catch (err) {
+      res.status(400).send({
+        message: err.message,
+      });
+    }
+  });
+
+  /* app.get("/movie/search", async (req: Request, res: Response) => {
+    try {
+      const movies = await searchMovie(req.query.query as string);
+  
+      res.status(200).send({
+        movies: movies,
+      });
+    } catch (err) {
+      res.status(400).send({
+        message: err.message,
+      });
+    }
+  }); */
 
 
 
