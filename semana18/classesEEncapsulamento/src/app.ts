a57daac384727a50c1ddd8bb2f842dd7ca59fd9c
artagnancoder <stayhomer@outlook.com>
2021-08-30 15:19:27 -0300

    Resolução do exercício 1

--- semana18/classesEEncapsulamento/src/app.ts
+++ semana18/classesEEncapsulamento/src/app.ts
@@ -1,13 +1,13 @@
-import express, {Express} from "express"
-import cors from "cors"
+import express from 'express'
+import cors from 'cors'
 
-const app: Express = express()
+const app = express()
 
 app.use(express.json())
 app.use(cors())
 
 app.listen(3003, ()=>{
-   console.log("Server ready!")
+    console.log('Servidor rodando na porta 3003')
 })
 
 export default app
--- semana18/classesEEncapsulamento/src/index.ts
+++ semana18/classesEEncapsulamento/src/index.ts
@@ -1,1 +1,52 @@
-import app from "./app"
+import { Transaction } from "knex"
+
+/* 
+
+Exercício 1
+
+a) O constructor é um método usado para criar e inicializar um objeto criado a partir de uma classe. Para chama-lo usamos a keyword New.
+
+*/
+
+
+// b) A mensagem "Chamando o construtor da classe User" foi impressa no terminal apenas uma vez.
+
+class UserAccount {
+    
+    private  cpf: string;
+    private name: string;
+    private age: number;
+    private balance: number = 0;
+    private transactions: Transaction[] = [];
+  
+    constructor(
+       userCpf: string,
+       userName: string,
+       userAge: number,
+    ) {
+       console.log("Chamando o construtor da classe UserAccount")
+       this.cpf = userCpf;
+       this.name = userName;
+       this.age = userAge;
+    };
+
+    
+     getCpf() {
+        return this.cpf;
+    }; 
+     
+  }
+
+  const cpf = "158946788-01"
+  const name = "Zé José"
+  const idade = 24
+
+  const zeJose = new UserAccount(cpf, name, idade)
+
+//c) 
+
+console.log(zeJose.getCpf())
+
+ 
+
+
