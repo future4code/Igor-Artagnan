2ec0feb91e02b755ab8efdcb2e5e8485c95380b3
artagnancoder <stayhomer@outlook.com>
2021-08-31 16:55:51 -0300

    .

--- semana18/herançaPolimorfismo/src/index.ts
+++ semana18/herançaPolimorfismo/src/index.ts
@@ -203,6 +203,68 @@ class User {
     }
   }
   
+  class ResidentialClient extends Residence implements Client {
+    constructor(
+      public name: string,
+      public registrationNumber: number,
+      public consumedEnergy: number,
+      private cpf: string,
+      residentsQuantity: number,
+      cep: string
+    ) {
+      super(residentsQuantity, cep);
+    }
+  
+    public getCpf(): string {
+      return this.cpf;
+    }
+  
+    public calculateBill(): number {
+      return this.consumedEnergy * 0.75;
+    }
+  }
+
+  class CommercialClient extends Commerce implements Client {
+    constructor(
+      public name: string,
+      public registrationNumber: number,
+      public consumedEnergy: number,
+      private cnpj: string,
+      floorsQuantity: number,
+      cep: string
+    ) {
+      super(floorsQuantity, cep);
+    }
+  
+    public calculateBill(): number {
+      return this.consumedEnergy * 0.53;
+    }
+  
+    public getCnpj(): string {
+      return this.cnpj;
+    }
+  }
+
+  class IndustrialClinet extends Industry implements Client {
+    constructor(
+      public name: string,
+      public registrationNumber: number,
+      public consumedEnergy: number,
+      private insdustryNumber: string, 
+      machinesQuantity: number,
+      cep: string
+    ) {
+      super(machinesQuantity, cep);
+    }
+  
+    public getIndustryNumber(): string {
+      return this.insdustryNumber;
+    }
+  
+    public calculateBill(): number {
+      return this.consumedEnergy * 0.45 + this.machinesQuantity * 100;
+    }
+  }
   
   
 
