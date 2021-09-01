07e372f2130e6c664bf21e20899cf231347ee14a
artagnancoder <stayhomer@outlook.com>
2021-09-01 13:43:36 -0300

    Criação do servidor e configuração das bibliotecas

---
+++ semana18/LabECommerceBackEnd/src/endpoints/createUser.ts
@@ -0,0 +1,24 @@
+/* import { Request, Response } from "express";
+import { connection } from "../data/connection";
+
+
+export default async function createUser(
+   req: Request,
+   res: Response
+): Promise<void> {
+   try {
+
+
+   } catch (error) {
+
+      if (typeof error === "string") {
+
+         res.send(error)
+      } else {
+         
+         console.log(error.sqlMessage || error.message);
+         res.status(500).send("Ops! Um erro inesperado ocorreu =/")
+      }
+
+   }
+} */
