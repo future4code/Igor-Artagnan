d7a9070a8275936bdde08b3a88cf366db83fe38d
artagnancoder <stayhomer@outlook.com>
2021-09-01 15:23:55 -0300

    Criação e configuração do arquivo Migrations. Inserção de tabelas no banco de dados.

--- semana18/LabECommerceBackEnd/src/data/migrations.ts
+++ semana18/LabECommerceBackEnd/src/data/migrations.ts
@@ -5,7 +5,39 @@ const printError = (error: any) => { console.log(error.sqlMessage || error.messa
 
 const createTables = () => connection
    .raw(`
-
+      
+      CREATE TABLE IF NOT EXISTS User(
+      id VARCHAR(255) PRIMARY KEY,
+      nome VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      idade INT NOT NULL
+      );
+      
+      CREATE TABLE IF NOT EXISTS Product(
+      id VARCHAR(255) PRIMARY KEY,
+      nome VARCHAR(255) NOT NULL,
+      descricao VARCHAR(255) NOT NULL,
+      preco INT NOT NULL
+      );
+      
+      CREATE TABLE IF NOT EXISTS Ticket(
+      id VARCHAR(255) PRIMARY KEY,
+      nome VARCHAR(255) NOT NULL,
+      descricao VARCHAR(255) NOT NULL,
+      preco INT NOT NULL,
+      origem VARCHAR(255) NOT NULL,
+      destino VARCHAR(255) NOT NULL
+      );
+      
+      CREATE TABLE IF NOT EXISTS Purchase(
+      id VARCHAR(255) PRIMARY KEY,
+      quantidade_itens INT NOT NULL,
+      valor_total INT NOT NULL,
+      product_id VARCHAR(255),
+      user_id VARCHAR(255),
+      FOREIGN KEY(user_id) REFERENCES User(id),
+      FOREIGN KEY(product_id) REFERENCES Product(id)
+      )
       
    `)
    .then(() => { console.log("Tabelas criadas") })
