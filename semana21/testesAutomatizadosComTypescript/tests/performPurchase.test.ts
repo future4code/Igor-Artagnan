c2957cc95bc69b2d4043e8c547e32db3f61923db
artagnancoder <stayhomer@outlook.com>
2021-09-21 12:55:03 -0300

    .

--- semana21/testesAutomatizadosComTypescript/tests/performPurchase.test.ts
+++ semana21/testesAutomatizadosComTypescript/tests/performPurchase.test.ts
@@ -4,17 +4,17 @@ import { Account } from "../src/types"
 //a)
 
 test("Testing balance greater than value", () => {
-	const account: Account = {
-		name: "John Doe",
-		balance: 100
-	}
+    const account: Account = {
+        name: "John Doe",
+        balance: 100
+    }
 
-	const result = performPurchase(account, 40)
-	
-	expect(result).toEqual({
-		name: "John Doe",
-		balance: 60
-	})
+    const result = performPurchase(account, 40)
+
+    expect(result).toEqual({
+        name: "John Doe",
+        balance: 60
+    })
 })
 
 //b)
@@ -28,9 +28,9 @@ test("Testing balance equal than value", () => {
     const result = performPurchase(account, 150)
 
     expect(result).toEqual({
-		...account,
-		balance: 0
-	})
+        ...account,
+        balance: 0
+    })
 })
 
 //c)
@@ -45,6 +45,6 @@ test("Testing balance less than value", () => {
     const result = performPurchase(account, 200)
 
     expect(result).not.toBeDefined()
-    
+
 })
 
