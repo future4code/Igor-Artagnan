0ba50cbf5feb3593b0182ef935bc551aeeeec7d1
artagnancoder <stayhomer@outlook.com>
2021-10-04 16:02:07 -0300

    Resolução do segundo exercício

--- semana23/resolvendoProblemas/src/exercise2/index.ts
+++ semana23/resolvendoProblemas/src/exercise2/index.ts
@@ -1,1 +1,28 @@
 //Implemente um método que performe uma compressão de string usando a contagem dos caracteres repetidos em sequência. Caso o resultado final tenha um tamanho maior do que a string inicial, seu programa deve retornar a string inicial
+
+const verifyChar = (string: string): string => {
+
+    const substrings = []
+    let lastChar = string[0]
+    let counter = 0
+
+    for (const char of string) {
+        if (char !== lastChar) {
+            substrings.push(lastChar + counter)
+            lastChar = char
+            counter = 0
+        }
+
+        counter++
+    }
+
+    substrings.push(lastChar + counter)
+    let result = ""
+
+    for (const key of substrings) {
+        result += key
+    }
+
+    return result.length > string.length ? string : result
+
+}
