3878463a077316ca637f8494a935e43ae84a50f0
artagnancoder <stayhomer@outlook.com>
2021-10-11 14:13:33 -0300

    Inicio do exercício

---
+++ semana24/exercicio1/index.js
@@ -0,0 +1,8 @@
+//### Exercício do dia - Número Faltante
+
+//Implemente uma função que receba um array (ordenado ou não) com números de 1 a 100. Você sabe que, nesse array, está faltando apenas um número dentro desse intervalo. A sua função deve retornar o número faltante. 
+
+//- Para a entrada `[1, 2, 3, ... , 45, 46, 48, 49, ... , 100]`, o retorno deve ser `47`
+//- Para a entrada `[1, 2, 3, ... , 69, 71, 72, ... , 100]`, o retorno deve ser `70`
+
+console.log('test')
