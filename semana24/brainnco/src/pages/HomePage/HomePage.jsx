26b0ca6534224c973635c96a6062c6a522317749
artagnancoder <stayhomer@outlook.com>
2021-10-13 10:58:50 -0300

    Integração do consumo dos endpoints.

--- semana24/brainnco/src/pages/HomePage/HomePage.jsx
+++ semana24/brainnco/src/pages/HomePage/HomePage.jsx
@@ -1,44 +1,66 @@
 import React, { useEffect, useState } from 'react'
-import { getLoterias } from '../../services/get'
-import InputLabel from '@mui/material/InputLabel';
+import { getLoterias, getConcursos, getResultados } from '../../services/get'
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-
 const HomePage = () => {
 
-    const [loterias, setLoterias] = useState([])
-     const [id, setId] = useState('');
+    const [loterias, setLoterias] = useState([]);
+    const [id, setId] = useState('');
+    const [concursos, setConcursos] = useState([]);
+    const [resultados, setResultados] = useState([]);
 
     const handleChange = (event) => {
         setId(event.target.value);
-    }; 
+    };
+
+    let concursoAtual = concursos.filter((concurso) => {
+        return concurso.loteriaId === id
+    })
+
+    let selected = concursoAtual[0].concursoId
 
     useEffect(() => {
         getLoterias(setLoterias)
-    }, [])
+        getConcursos(setConcursos)
+        getResultados(setResultados, selected)
+    }, [id])
+
+
+
 
     return <div>
         HOME
         {console.log(loterias)}
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
-           
-           {/*  <Select
-                value={age}
-                onChange={handleChange} 
-                label="Age"
-            > */}
-               {/*  <MenuItem value="">
-                    <em>None</em>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+
+            <Select
+                value={id}
+                onChange={handleChange}
+            >
+                <MenuItem value="">
+
                 </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem> */}
+                {loterias.map((x) => {
+                    return <MenuItem
+                        value={x.id}>{x.nome}</MenuItem>
+                })}
+
             </Select>
 
         </FormControl>
+
+        {console.log('VALUE', id)}
+        {console.log('Concursos', concursos)}
+        {console.log('ATUAL', concursoAtual)}
+        {console.log('SELECTED', selected)}
+        {console.log('RESULTADOS', resultados)}
+
+
+
+        <p>Este sorteio é meramente ilustrativo e não possui nenhuma ligação com a CAIXA.</p>
+
     </div>
 }
 
